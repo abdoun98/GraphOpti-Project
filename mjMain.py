6596976414845de64c6ba5a20fd0f36896dfcdff
MaelJaouen <maeljaouen012@gmail.com>
2020-11-24 19:04:53 +0100

    implementation de evalue, voisinageVariable, et solutionAleatoire

--- mjMain.py
+++ mjMain.py
@@ -1,4 +1,8 @@
 import math
+import random
+import numpy as np
+import copy
+
 # This is a sample Python script.
 
 # Press Maj+F10 to execute it or replace it with your code.
@@ -6,6 +10,66 @@ import math
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+
+    def permuteRing(listeRing, i):
+        listeRing[i], listeRing[i - 1] = listeRing[i - 1], listeRing[i]
+        return listeRing
+
+    def entreRing(listeRing, listeHorsRing, listeLienHorsRing, i, j):
+        listeRing = listeRing[:j] + [listeHorsRing[i]] + listeRing[j:]
+        listeHorsRing.remove(listeHorsRing[i])
+        listeLienHorsRing.remove(listeLienHorsRing[i])
+        attributSommetRing(listeRing, listeHorsRing, listeLienHorsRing)
+
+
+    def attributSommetRing(listeRing, listeHorsRing, listeLienHorsRing):
+        for i in range(len(listeHorsRing)):
+            m = math.inf
+            s = -1
+            for j in range(len(listeRing)):
+                if m > Ca[listeHorsRing[i]][listeRing[j]]:
+                    m = Ca[listeHorsRing[i]][listeRing[j]]
+                    s = listeRing[j]
+            listeLienHorsRing[i] = s
+
+
+    def solutionAleatoire():
+        listeHorsRing = np.array(np.linspace(0, len(Ca), len(Ca), endpoint=False), dtype='int').tolist()
+        N = random.randint(0, len(Ca))
+        listeRing = []
+        for i in range(N):
+            n = random.randint(0, len(Ca) - i -1)
+            listeRing.append(listeHorsRing[n])
+            listeHorsRing.remove(listeHorsRing[n])
+        listeLienHorsRing = np.zeros(len(listeHorsRing))
+        attributSommetRing(listeRing, listeHorsRing, listeLienHorsRing)
+        return listeRing, listeHorsRing, listeLienHorsRing
+
+    def evalue(listeRing, listeHorsRing, listeLienHorsRing):
+        c=0
+        for i in range(len(listeRing)):
+            c+=Cr[listeRing[i-1]][listeRing[i]]
+        for j in range(len(listeHorsRing)):
+            c+=Ca[int(listeHorsRing[j])][int(listeLienHorsRing[j])]
+        return c
+
+    def voisinageVariable(N):
+        meilleurCout=math.inf
+        mListeRing=[]
+        mListeHorsRing=[]
+        mListeLienHorsRing=[]
+        for i in range(N):
+            listeRing, listeHorsRing, listeLienHorsRing = solutionAleatoire()
+            for j in range(len(listeRing)):
+                copListeRing=copy.deepcopy(listeRing)
+                copListeRing=permuteRing(copListeRing, j)
+                cout=evalue(copListeRing, listeHorsRing, listeLienHorsRing)
+                if cout<meilleurCout:
+                    meilleurCout=cout
+                    mListeRing=copy.deepcopy(copListeRing)
+                    mListeHorsRing=copy.deepcopy(listeHorsRing)
+                    mListeLienHorsRing=copy.deepcopy(listeLienHorsRing)
+        return meilleurCout, mListeRing, mListeHorsRing,mListeLienHorsRing
 
     Cr = []  # cout du ring
     Ca = []  # cout des liens vers ring
@@ -31,26 +95,6 @@ if __name__ == '__main__':
 
         print(Ca[1][2])
         print("fin d'extraction des donnees")
-
-
+        meilleurCout, mListeRing, mListeHorsRing,mListeLienHorsRing = voisinageVariable(10000)
+        print(meilleurCout)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-def permuteRing(listeRing, i):
-    listeRing[i], listeRing[i - 1] = listeRing[i - 1], listeRing[i]
-
-def entreRing(listeRing,listeHorsRing,listeLienHorsRing,i,j):
-    listeRing=listeRing[:j]+[listeHorsRing[i]]+listeRing[j:]
-    listeHorsRing=listeHorsRing[:i]+listeHorsRing[i+1:]
-    listeLienHorsRing=listeLienHorsRing[:i]+listeLienHorsRing[i+1:]
-    attributSommetRing(listeRing,listeHorsRing,listeLienHorsRing)
-
-def attributSommetRing(listeRing,listeHorsRing,listeLienHorsRing):
-    for i in len(listeHorsRing):
-        m=math.inf
-        s=-1
-        for j in len(listeRing):
-            if m>Ca[i][j]:
-                m=Ca[i][j]
-                s=j
-        listeLienHorsRing[i]=s
