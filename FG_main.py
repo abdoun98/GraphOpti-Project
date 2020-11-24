3d1d713836d68ab1d8b5171731c9be6eb19ea85e
Francois.Geremie <francois.geremie@student.umons.ac.be>
2020-11-24 16:20:03 +0100

    avec le main

--- FG_main.py
+++ FG_main.py
@@ -1,25 +1,28 @@
-Cr = [] #cout du ring
-Ca = [] #cout des liens vers ring
 
-with open("Datasets/data1.txt") as f:
-    data = f.readline()
-    data = data.split()
-    data = list(map(int, data))
-    N = data[0] #Nombre de sommets du problème
-    print(N)
+if __name__ == '__main__':
+    Cr = [] #cout du ring
+    Ca = [] #cout des liens vers ring
+
+    with open("Datasets/data1.txt") as f:
+        data = f.readline()
+        data = data.split()
+        data = list(map(int, data))
+        N = data[0] #Nombre de sommets du problème
+        print(N)
 
 
-    for i in range (N):
-        sommet = f.readline()
-        sommet = sommet.split()
-        sommet = list(map(int,sommet))
-        Cr.append(sommet)
+        for i in range (N):
+            sommet = f.readline()
+            sommet = sommet.split()
+            sommet = list(map(int,sommet))
+            Cr.append(sommet)
 
-    for j in range(N):
-        sommet2 = f.readline()
-        sommet2 = sommet2.split()
-        sommet2 = list(map(int,sommet2))
-        Ca.append(sommet2)
+        for j in range(N):
+            sommet2 = f.readline()
+            sommet2 = sommet2.split()
+            sommet2 = list(map(int,sommet2))
+            Ca.append(sommet2)
 
-    print(Ca[1][2])
-    print("fin d'extraction des donnees")
+        print(Ca[1][2])
+        print("fin d'extraction des donnees")
+
