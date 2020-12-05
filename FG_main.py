7654dbb22fa95f312937858a3c4a6a1e2d407e87
Francois.Geremie <francois.geremie@student.umons.ac.be>
2020-12-05 11:05:35 +0100

    evolu quasi fini

--- FG_main.py
+++ FG_main.py
@@ -31,7 +31,7 @@ def initiaterandom(N):
     STAR = np.array(np.linspace(2, N + 1, (N - 1), endpoint=False),
                     dtype='int').tolist()  # linspace(START, STOP (on ne prend pas cette valeur), nrbe éléments)
     # print(STAR)
-    # nombre d'éléments à switch entre RING et STAR, donc bien de 0 -> N-1 (le ring doit contenir min 1 elem)
+    # nombre d'éléments à switch entre STAR et RING, donc bien de 0 -> N-1 (le ring doit contenir min 1 elem)
     nbr = random.randint(0, N - 1)
     for i in range(nbr):
         # numéro du sommet à switch
@@ -46,25 +46,19 @@ def initiaterandom(N):
 ################
 # Retourne le coût total et les arcs optimaux (PEER) pour le STAR
 def evaluate(RING, STAR, N, Cr, Ca):
-    c = 0
+    c = Cr[0][RING[-1] - 1]
+    #print("cout initial : ", c)
     PEER = []  # Contient les arcs optimaux pour STAR
     # Coût du RING
     for i in range(len(RING) - 1):
         c += Cr[RING[i] - 1][RING[i + 1] - 1]
+        #print("update cout r ({}) : {}".format(i, c))
     # MAJ de PEER et du coût
-    for e in STAR:
-        cmin = Ca[e - 1][0]
-        i_min = 0
-        for i in range(N):
-            if Ca[e - 1][i] == 0 and i == 0:  # dans le cas où 1 est dans le STAR
-                cmin = Ca[e - 1][i + 1]
-            elif Ca[e - 1][i] == 0:
-                continue
-            elif Ca[e - 1][i] < cmin:
-                cmin = Ca[e - 1][i]
-                i_min = i
-        PEER.append([e, i_min + 1])
-        c += cmin
+    for i in range(len(STAR) - 1):
+        temp = random.choice(RING)
+        PEER.append([STAR[i], temp])
+        c += Ca[STAR[i]][temp - 1]
+        #print("update cout hr ({}) : {}".format(i, c))
     return c, PEER
 
 
@@ -92,41 +86,68 @@ def evolutionnaire(N, Cr, Ca):
         c, PEER = evaluate(RING, STAR, N, Cr, Ca)
         Population.append(Individu(RING, STAR, PEER, c))
 
-    # Sélection
-    Couple = []
-    pop = [i for i in range(0, T)]
-    for i in range(int(T/2)):
-        A = random.choice(pop)
-        pop.remove(A)
-        B = random.choice(pop)
-        pop.remove(B)
-        Couple.append([Population[A], Population[B]])
+    for g in range(G):  # G générations se succèdent
 
-    # Croisement
-    for i in range(int(T/2)):
-        p1 = Couple[i][0].RING
-        p2 = Couple[i][1].RING
-        e1 = [1]
-        e2 = [1]
-        if len(p1) == 1 or len(p2) == 1:  # si l'indidividu est de taille 1, on ne croise pas
-            continue
-        else:
-            # enfant 1
-            e1.append(p2[-1])  # on choisit un le dernier elem p2 à introduire dans e1
-            for j in range(2, len(p1)):  # on construit e1 avec les éléments de p1
-                if e1[1] == p1[j]:  # l'élément switch ne peut pas se retrouver 2 fois dans e1
-                    continue
-                else:
-                    e1.append(p1[j])
-            # enfant 2
-            e2.append(p1[-1])
-            for j in range(2, len(p2)):
-                if e2[1] == p2[j]:
-                    continue
-                else:
-                    e2.append(p2[j])
-        print("p1 : " + str(p1) + "\n" + "p2 : " + str(p2) + "\n" + "e1 : " + str(e1) + "\n" + "e2 : " + str(e2) + "\n")
+        # Sélection
+        Couple = []
+        pop = [i for i in range(0, T)]  # liste des indices des individus
+        for i in range(int(T/2)):
+            A = random.choice(pop)
+            pop.remove(A)
+            B = random.choice(pop)
+            pop.remove(B)
+            Couple.append([Population[A], Population[B]])
 
+        # Croisement
+        Enfant = []
+        for i in range(int(T/2)):
+            p1 = Couple[i][0].RING
+            p2 = Couple[i][1].RING
+            e1 = [1]  # chaque solution doit commencer par 1
+            e2 = [1]
+            if len(p1) == 1 or len(p2) == 1:  # si l'indidividu est de taille 1, on ne croise pas
+                continue
+            else:
+                # enfant 1
+                e1.append(p2[-1])  # on choisit le dernier elem p2 à introduire dans e1
+                for j in range(2, len(p1)):  # on construit e1 avec les éléments de p1
+                    if e1[1] == p1[j]:  # l'élément switch ne peut pas se retrouver 2 fois dans e1
+                        continue
+                    else:
+                        e1.append(p1[j])
+                Enfant.append(e1)
+                # enfant 2
+                e2.append(p1[-1])
+                for j in range(2, len(p2)):
+                    if e2[1] == p2[j]:
+                        continue
+                    else:
+                        e2.append(p2[j])
+                Enfant.append(e2)
+            print("p1 : " + str(p1) + "\n" + "p2 : " + str(p2) + "\n" + "e1 : " + str(e1) + "\n" + "e2 : " + str(e2) + "\n")
+
+
+        # Permutation
+        for i in range(len(Enfant)):
+            if len(Enfant[i]) == 2:  # si Enfant est de taille 2, on ne switch pas car 1 toujours en 1ère place
+                continue
+            else:
+                n = random.randint(1, len(Enfant[i]) - 2)  # indice du somment à switch (pas 0 et pas dernier)
+                Enfant[i][n], Enfant[i][n+1] = Enfant[i][n+1], Enfant[i][n]
+
+        # Construction des individus Enfants
+        for i in range(len(Enfant)):
+            STAR = []
+            for j in range(2, N+1):
+                if j not in Enfant[i]:  # possibilité d'être plus opti à voir plus tard
+                    STAR.append(j)
+            c, PEER = evaluate(Enfant[i], STAR, N, Cr, Ca)
+            Population.append(Individu(Enfant[i], STAR, PEER, c))  # T parents suivis de T enfants -> sélection à faire
+
+        # Sélection d'individus
+        Population = sorted(Population, key=lambda x: x.Cost)  # Tri des individus de Population selon leur score
+        for i in range(T):
+            Population.remove(Population[-1])  # on supprime les individus avec le score le plus élevé
 
     # Affichage
     #for i in range(T):
