5052ab3e19edac157083d59087877a330c428790
Abderrahmen Abdoun <abderrahmen.abdoun@student.umons.ac.be>
2020-12-05 15:14:49 +0100

    permutation1

--- FG_main.py
+++ FG_main.py
@@ -46,18 +46,18 @@ def initiaterandom(N):
 # Retourne le coût total et les arcs optimaux (PEER) pour le STAR
 def evaluate(RING, STAR, N, Cr, Ca):
     c = Cr[0][RING[-1] - 1]
-    #print("cout initial : ", c)
+    # print("cout initial : ", c)
     PEER = []  # Contient les arcs optimaux pour STAR
     # Coût du RING
     for i in range(len(RING) - 1):
         c += Cr[RING[i] - 1][RING[i + 1] - 1]
-        #print("update cout r ({}) : {}".format(i, c))
+        # print("update cout r ({}) : {}".format(i, c))
     # MAJ de PEER et du coût
     for i in range(len(STAR) - 1):
         temp = random.choice(RING)
         PEER.append([STAR[i], temp])
         c += Ca[STAR[i]][temp - 1]
-        #print("update cout hr ({}) : {}".format(i, c))
+        # print("update cout hr ({}) : {}".format(i, c))
     return c, PEER
 
 
@@ -70,6 +70,57 @@ class Individu:
         self.STAR = STAR
         self.PEER = PEER
         self.Cost = Cost
+
+
+def croisement1(T, Pc, Couple):
+    enfants = []
+    for i in range(int(T / 2)):
+        chance = random.random()  # chance in [0,1] car pas sûr de croiser
+        if chance > Pc:
+            print("Pas croisement" + "\n")
+            continue  # il n'y a pas de reproduction
+
+        else:  # il y a reproduction (et donc croisement des caractéristiques)
+            p1 = Couple[i][0].RING
+            p2 = Couple[i][1].RING
+            e1 = [1]  # chaque solution doit commencer par 1
+            e2 = [1]
+            if len(p1) == 1 or len(p2) == 1:  # si l'indidividu est de taille 1, on ne croise pas
+                continue
+            else:
+                # enfant 1
+                e1.append(p2[-1])  # on choisit le dernier elem p2 à introduire dans e1
+                for j in range(2, len(p1)):  # on construit e1 avec les éléments de p1
+                    if e1[1] == p1[j]:  # l'élément switch ne peut pas se retrouver 2 fois dans e1
+                        continue
+                    else:
+                        e1.append(p1[j])
+                enfants.append(e1)
+                # enfant 2
+                e2.append(p1[-1])
+                for j in range(2, len(p2)):
+                    if e2[1] == p2[j]:
+                        continue
+                    else:
+                        e2.append(p2[j])
+                enfants.append(e2)
+            print("p1 : " + str(p1) + "\n" + "p2 : " + str(p2) + "\n" + "e1 : " + str(e1) + "\n" + "e2 : " + str(
+                e2) + "\n")
+    return enfants
+
+def permutation1(Pm, Enfant):
+    for i in range(len(Enfant)):
+        chance = random.random()
+        if chance > Pm:
+            continue  # il n'y a pas de mutation
+
+        else:  # il y a mutation chez l'enfant
+            if len(Enfant[i]) == 2:  # si Enfant est de taille 2, on ne permute pas car 1 toujours en 1ère place
+                continue
+            else:
+                n = random.randint(1, len(Enfant[i]) - 2)  # indice du somment à permuter (pas 0 et pas dernier)
+                Enfant[i][n], Enfant[i][n + 1] = Enfant[i][n + 1], Enfant[i][n]
+    return Enfant
 
 
 def evolutionnaire(N, Cr, Ca):
@@ -91,65 +142,23 @@ def evolutionnaire(N, Cr, Ca):
         # Sélection des couples
         Couple = []
         pop = [i for i in range(0, T)]  # liste des indices des individus
-        for i in range(int(T/2)):
+        for i in range(int(T / 2)):
             A = random.choice(pop)
             pop.remove(A)
             B = random.choice(pop)
             pop.remove(B)
             Couple.append([Population[A], Population[B]])
 
-
         # Croisement
-        Enfant = []
-        for i in range(int(T/2)):
-            chance = random.random()  # chance in [0,1] car pas sûr de croiser
-            if chance > Pc:
-                print("Pas croisement" + "\n")
-                continue  # il n'y a pas de reproduction
-
-            else:  # il y a reproduction (et donc croisement des caractéristiques)
-                p1 = Couple[i][0].RING
-                p2 = Couple[i][1].RING
-                e1 = [1]  # chaque solution doit commencer par 1
-                e2 = [1]
-                if len(p1) == 1 or len(p2) == 1:  # si l'indidividu est de taille 1, on ne croise pas
-                    continue
-                else:
-                    # enfant 1
-                    e1.append(p2[-1])  # on choisit le dernier elem p2 à introduire dans e1
-                    for j in range(2, len(p1)):  # on construit e1 avec les éléments de p1
-                        if e1[1] == p1[j]:  # l'élément switch ne peut pas se retrouver 2 fois dans e1
-                            continue
-                        else:
-                            e1.append(p1[j])
-                    Enfant.append(e1)
-                    # enfant 2
-                    e2.append(p1[-1])
-                    for j in range(2, len(p2)):
-                        if e2[1] == p2[j]:
-                            continue
-                        else:
-                            e2.append(p2[j])
-                    Enfant.append(e2)
-                print("p1 : " + str(p1) + "\n" + "p2 : " + str(p2) + "\n" + "e1 : " + str(e1) + "\n" + "e2 : " + str(e2) + "\n")
+        Enfant = croisement1(T, Pc, Couple)
 
         # Permutation
-        for i in range(len(Enfant)):
-            chance = random.random()
-            if chance > Pm:
-                continue  # il n'y a pas de mutation
-
-            else:  # il y a mutation chez l'enfant
-                if len(Enfant[i]) == 2:  # si Enfant est de taille 2, on ne permute pas car 1 toujours en 1ère place
-                    continue
-                else:
-                    n = random.randint(1, len(Enfant[i]) - 2)  # indice du somment à permuter (pas 0 et pas dernier)
-                    Enfant[i][n], Enfant[i][n+1] = Enfant[i][n+1], Enfant[i][n]
+        Enfant = permutation1(Pm, Enfant)
 
         # Construction des individus Enfants
         for i in range(len(Enfant)):
             STAR = []
-            for j in range(2, N+1):
+            for j in range(2, N + 1):
                 if j not in Enfant[i]:  # possibilité d'être plus opti à voir plus tard
                     STAR.append(j)
             c, PEER = evaluate(Enfant[i], STAR, N, Cr, Ca)
@@ -161,12 +170,13 @@ def evolutionnaire(N, Cr, Ca):
             Population.remove(Population[-1])  # on supprime les individus avec le score le plus élevé
 
     # Affichage
-    #"""
+    # """
     print("\n" + "Individus sélectionnés")
     for i in range(T):
-        print("Individu " + str(i + 1) + "\n" + "RING : " + str(Population[i].RING) + "\n" + "STAR : " + str(Population[i].STAR) + "\n" + "PEER : "
+        print("Individu " + str(i + 1) + "\n" + "RING : " + str(Population[i].RING) + "\n" + "STAR : " + str(
+            Population[i].STAR) + "\n" + "PEER : "
               + str(Population[i].PEER) + "\n" + "Cost : " + str(Population[i].Cost) + "\n")
-    #"""
+    # """
     return Population
 
 
@@ -205,19 +215,18 @@ if __name__ == '__main__':
     ##############
     # Résolution #
     ##############
-    #listeRing, listeHorsRing = initiate(N)
+    # listeRing, listeHorsRing = initiate(N)
     listeRing, listeHorsRing = initiaterandom(N)
     cost, listeLienHorsRing = evaluate(listeRing, listeHorsRing, N, Cr, Ca)
 
     population = evolutionnaire(N, Cr, Ca)
     print(population)
 
-
     #####################
     # Affichage et test #
     #####################
 
-    #print("RING : " + str(listeRing) + "\n" + "STAR : " + str(listeHorsRing) + "\n" + "PEER : " + str(listeLienHorsRing) + "\n" + "Cost : " + str(cost))
+    # print("RING : " + str(listeRing) + "\n" + "STAR : " + str(listeHorsRing) + "\n" + "PEER : " + str(listeLienHorsRing) + "\n" + "Cost : " + str(cost))
 
     """
     #Test des éléments
@@ -230,6 +239,5 @@ if __name__ == '__main__':
     for b in listeHorsRing:
         sum1 += b
     print(sum, sum1)
-    
     print("end")
     #"""
